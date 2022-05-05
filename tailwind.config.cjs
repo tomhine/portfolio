e9d52ecf20ea01f630b8ef9b714f224b677e3684
Tom Hine <tomhine792@gmail.com>
2022-05-05 15:51:13 +0100

    added font and primary hover color

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,11 +5,15 @@ module.exports = {
       colors: {
         primary: {
           orange: "#e64b4a",
+          orangehover: '#b6352b',
         },
         neutral: {
           light: "#30353c",
           dark: "#1a1c23",
         },
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
       },
     },
   },
