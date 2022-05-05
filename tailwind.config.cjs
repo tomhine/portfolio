91a4926eabe6badd1b951cf3ef6d00c07a510cb6
Tom Hine <tomhine792@gmail.com>
2022-05-05 07:17:46 +0100

    first commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          orange: "#e64b4a",
+        },
+        neutral: {
+          light: "#30353c",
+          dark: "#1a1c23",
+        },
+      },
+    },
+  },
+  plugins: [],
+};
